6752b8cc3c7e56d06de278f240613975c28e2799
LawrenceChou19 <LawrenceChou19@gmail.com>
2023-01-24 08:38:29 +0800

    2023-01-24-class

--- W5/W5-01-01-Practice.py
+++ W5/W5-01-01-Practice.py
@@ -1,4 +1,38 @@
 print(type(0))
 print(type("")) 
 print(dir(""))
-print(help("int"))
+
+# print(help("int"))
+
+class Apple:
+    pass
+
+class Apple:
+    color = ""
+    flavor = ""
+
+jonagold= Apple()
+jonagold.color = "red"
+jonagold.flavor = "sweet"
+print(jonagold.color)
+print(jonagold.flavor)
+
+goldem =  Apple()
+goldem.color = "Yellow"
+goldem.flavor = "Soft"
+
+
+class Flower:
+  color = 'unknown'
+
+rose = Flower()
+rose.color = "Red"
+
+violet = Flower()
+violet.color = "Blue"
+
+this_pun_is_for_you = "Sugar is sweet, and so are you."
+
+print("Roses are {},".format(rose.color))
+print("violets are {},".format(violet.color))
+print(this_pun_is_for_you) 
