ed8092d54f5c809d455b5f90bc5c3fdb3401e630
LawrenceChou11 <42228071+LawrenceChou11@users.noreply.github.com>
2023-01-26 07:47:49 +0800

    2023-01-26

--- W5/W5-01-01-Practice.py
+++ W5/W5-01-01-Practice.py
@@ -235,7 +235,33 @@ print(describe_furniture(couch))
 
 
 
+#Practice
+# class Piglet:
+#     def speak(self):
+#         print("oink oink")
+
+# hamlet = Piglet()
+# hamlet.speak()
 
 
+class Piglet:
+    name = "piglet"
+    def speak(self):
+        print("Oink! I'm {}! Oink!".format(self.name))
+hamlet = Piglet()
+hamlet.name = "Hamlet"
+hamlet.speak()
 
+petunia = Piglet()
+petunia.name ="Petunia"
+petunia.speak()
 
+class Piglet:
+    years = 0
+    def pig_years(self):
+        return self.years * 18
+
+hamlet = Piglet()
+print(hamlet.pig_years())
+hamlet.years = 2
+print(hamlet.pig_years())
