30395bbc22d78e7f12c3f68cad037f27caa66a83
LawrenceChou19 <LawrenceChou19@gmail.com>
2023-01-29 16:35:01 +0800

    2023-01-29

--- W5/W5-01-01-Practice.py
+++ W5/W5-01-01-Practice.py
@@ -323,3 +323,59 @@ class Person:
 		"""Outputs a message with the name of the person"""
 		print("Hello! My name is {name}.".format(name=self.name))     
 help(Person)
+
+class Elevator:
+	def __init__(self, bottom, top, current):
+		"""Initializes the Elevator instance."""
+		self.bottom = bottom #-1
+		self.top = top #10
+		self.current = current #0
+
+	def up(self):
+		"""Makes the elevator go up one floor."""
+		# print('current is',self.current)
+		if self.current < self.top: # 0 vs 10
+			self.current += 1 #1
+			# print('current is',self.current)
+		else: 
+			self.current #0
+
+	def down(self):
+		"""Makes the elevator go down one floor."""
+		# print('current is',self.current)
+		if self.current > self.bottom: # 1 vs -1
+			self.current -= 1
+			# print('current is',self.current)
+		else: 
+			self.current 
+
+	def go_to(self, floor):
+		"""Makes the elevator go to the specific floor."""
+		# print('Currnet floor is ',self.current)
+		self.current = floor
+
+	def current(self):
+		print('Current is',self.current)
+		return self.current
+
+elevator = Elevator(-1, 10, 0)
+
+elevator.up() 
+print(elevator.current) #should output 1
+elevator.down() 
+print(elevator.current) #should output 0
+elevator.go_to(10) 
+print(elevator.current) #should output 10
+
+# Go to the top floor. Try to go up, it should stay. Then go down.
+elevator.go_to(10)
+elevator.up()
+elevator.down()
+print(elevator.current) # should be 9
+# Go to the bottom floor. Try to go down, it should stay. Then go up.
+elevator.go_to(-1)
+elevator.down()
+elevator.down()
+elevator.up()
+elevator.up()
+print(elevator.current) # should be 1
